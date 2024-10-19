358bb826ae120d52a70db431762f14d80aa15425
Erdem <rifaterdemsahin@gmail.com>
2024-10-19 17:55:20 +0000

    Refined

--- SymbolicCode/portforward.sh
+++ SymbolicCode/portforward.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Port forward Prometheus server
-kubectl port-forward svc/prometheus-server 9090:9090 &
+kubectl port-forward svc/prometheus-server 9090:80 &
 
 # Port forward Grafana
 kubectl port-forward svc/grafana 3000:80 &
