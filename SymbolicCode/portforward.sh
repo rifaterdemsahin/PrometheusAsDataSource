cc8df997db3789e0f5715bb6777eab1bb710fde4
Erdem <rifaterdemsahin@gmail.com>
2024-10-26 13:41:40 +0000

    begum commit

--- SymbolicCode/portforward.sh
+++ SymbolicCode/portforward.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Port forward Prometheus server
-kubectl port-forward svc/prometheus-server 9090:80 &
+kubectl port-forward svc/prometheus-server 9090:80 -n monitoring &
 
 # Port forward Grafana
-kubectl port-forward svc/grafana 3000:80 &
+kubectl port-forward svc/grafana 3000:80 -n monitoring &
  
 # Wait for both processes to finish
 wait
